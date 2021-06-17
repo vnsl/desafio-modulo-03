03b7f57fe8e2bcccc797df11db525bddd27ad2aa
Vinicius <viniciusnattan14@gmail.com>
2021-06-17 16:29:34 -0300

    SQL

--- back-end/db.sql
+++ back-end/db.sql
@@ -6,35 +6,20 @@ create table usuarios (
   id serial primary key,
   nome varchar(60) NOT NULL,
   nome_loja text,
-  email integer NOT NULL,
-  senha varchar(20) NOT NULL,
+  email text NOT NULL unique,
+  senha text NOT NULL
 );
 
 drop table produtos if exists;
 
 create table produtos (
-  id serial NOT NULL primary key,
-  usuario_id serial NOT NULL references usuarios(id),
+  id serial primary key,
+  usuario_id integer NOT NULL,
   nome varchar(60) NOT NULL,
   estoque integer NOT NULL,
   categoria varchar(20) NOT NULL,
   preco integer NOT NULL,
   descricao text,
   imagem text,
-);
-
-insert usuarios values (
-    $1,
-    $2,
-    $3,
-    $4
-);
-
-insert produtos values (
-    $1,
-    $2,
-    $3,
-    $4,
-    $5,
-    $6,
+  foreign key (usuario_id) references usuarios (id)
 );
